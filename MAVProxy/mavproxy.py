1fd5106841793e28803d59ae585310638587c8d8
Seth Hilaski <hilaskis@mail.gvsu.edu>
2015-06-27 17:56:38 -0400

    Added line that periodically sends PI_PACKET message for testing

--- MAVProxy/mavproxy.py
+++ MAVProxy/mavproxy.py
@@ -675,6 +675,7 @@ def main_loop():
     if not mpstate.status.setup_mode and not opts.nowait:
         for master in mpstate.mav_master:
             send_heartbeat(master)
+            master.mav.pi_packet_send(30, 180, 0)
             if master.linknum == 0:
                 master.wait_heartbeat()
         set_stream_rates()
